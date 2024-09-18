2ca8e1adcdc1be627dc3522b27c7cfacd5e037ad
ZacharyWDWS <zwhitty11@gmail.com>
2024-09-17 22:58:13 -0500

    setting up api

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from './product.service';
+import { Product } from './product';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent implements OnInit {
+
+  products: Product[] = [];
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.getProductList();
+  }
+  getProductList() {
+    this.productService.getProducts().subscribe(
+      response => {
+        this.products = response;
+      }
+    )
+  }
+
+}
