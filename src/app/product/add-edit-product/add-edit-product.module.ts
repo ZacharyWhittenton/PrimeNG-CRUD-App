4d56c013378f5fb5fed5419b5892a04ddc335f41
ZacharyWDWS <zwhitty11@gmail.com>
2024-09-18 16:14:43 -0500

    added edit  componets

--- src/app/product/add-edit-product/add-edit-product.module.ts
+++ src/app/product/add-edit-product/add-edit-product.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AddEditProductComponent } from './add-edit-product.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddEditProductComponent
+  ],
   imports: [
     CommonModule
   ]
