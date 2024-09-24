d2148011c44c91947e44146ef4f295d129909100
ZacharyWDWS <zwhitty11@gmail.com>
2024-09-23 22:11:13 -0500

    addProduct build

--- src/app/product/add-edit-product/add-edit-product.component.ts
+++ src/app/product/add-edit-product/add-edit-product.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-add-edit-product',
@@ -7,11 +8,14 @@ import { FormBuilder, Validators, FormGroup } from '@angular/forms';
   styleUrls: ['./add-edit-product.component.css']
 })
 export class AddEditProductComponent {
+
   @Input() displayAddModal: boolean = true;
   @Output() clickClose: EventEmitter<boolean> = new EventEmitter<boolean>();
+
   productForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private productService: ProductService) {
+    // Initialize the form inside the constructor
     this.productForm = this.fb.group({
       title: ["", Validators.required],
       price: [0, Validators.required],
@@ -24,11 +28,15 @@ export class AddEditProductComponent {
   closeModal() {
     this.clickClose.emit(true);
   }
-  addProduct() {
-    if (this.productForm.valid) {
-      // Handle the logic for adding the product here
-      console.log(this.productForm.value); // Example of what to do with the data
 
-    }
+  addProduct() {
+    // Use productForm's value instead of productService's value
+    this.productService.saveProduct(this.productForm.value).subscribe(
+      response => {
+        console.log(response);
+        this.productForm.reset();
+        this.clickClose.emit(true);
+      }
+    );
   }
 }
